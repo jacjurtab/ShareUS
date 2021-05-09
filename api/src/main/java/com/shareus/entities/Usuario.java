c274c36049599f9849aca0abbcc25f7095ec5136
celllarod <celllarod@alum.us.es>
2021-05-09 17:43:23 +0200

    Creadas las entidades

---
+++ api/src/main/java/com/shareus/entities/Usuario.java
@@ -0,0 +1,63 @@
+package com.shareus.entities;
+
+public class Usuario {
+	
+	private int id;
+	private String clave;
+	private String nombre;
+	private String primer_apellido;
+	private String segundo_apellido;
+	private String email;
+	private String telefono;
+	private float valoracion;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPrimer_apellido() {
+		return primer_apellido;
+	}
+	public void setPrimer_apellido(String primer_apellido) {
+		this.primer_apellido = primer_apellido;
+	}
+	public String getSegundo_apellido() {
+		return segundo_apellido;
+	}
+	public void setSegundo_apellido(String segundo_apellido) {
+		this.segundo_apellido = segundo_apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public float getValoracion() {
+		return valoracion;
+	}
+	public void setValoracion(float valoracion) {
+		this.valoracion = valoracion;
+	}
+	
+}
