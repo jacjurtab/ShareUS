eda968df1c14c56d41d99ac17ce38866c53d1459
smartnexus <angrodboh@alum.us.es>
2021-05-13 16:29:47 +0200

    testing

--- app/src/main/java/com/example/shareus/ui/misviajes/tabview/ViajesConductorFragment.java
+++ app/src/main/java/com/example/shareus/ui/misviajes/tabview/ViajesConductorFragment.java
@@ -15,7 +15,7 @@ import com.example.shareus.R;
  * Use the {@link ViajesConductorFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ViajesConductorFragment extends Fragment {
+public class ViajesConductorFragment extends Fragmento {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
